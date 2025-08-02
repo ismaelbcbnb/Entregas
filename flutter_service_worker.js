3d63193d2cb1e96b92aa5d88d38983c243744bb7
ismaelbcbnb <ismaelbcbnb@users.noreply.github.com>
2025-08-02 07:20:25 +0000

    deploy: 60276a7363771ee9c8514c982050666c2283a817

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "683cd8f22eb3efd046d3139c218446cd
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "version.json": "bf84c00bce175d9eee0a154e909423a5",
 "manifest.json": "13898ac3d529feca6016f1beaf393c7d",
-"flutter_bootstrap.js": "e45bac3da42b0856ed2a1c83bb542cc8",
+"flutter_bootstrap.js": "ce1f7e12698b53c780d741dff802751e",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
